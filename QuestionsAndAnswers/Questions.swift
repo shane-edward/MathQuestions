a0d4ba755fb524f5c6d546c4379e57005d1fc0cc
Shane Blackburn <shane_blackburn@icloud.com>
2020-05-18 19:44:29 -0400

    Added another question to the questions array.

--- QuestionsAndAnswers/Questions.swift
+++ QuestionsAndAnswers/Questions.swift
@@ -21,5 +21,6 @@ let questions = [
     Question(question: "What is 4 + 4", answerOne: "8", answerTwo: "10", answerThre: "0", answerFour: "4", correctAnswerIndex: 0),
     Question(question: "What is the Square Root of 49", answerOne: "6", answerTwo: "2401", answerThre: "7", answerFour: "4.9", correctAnswerIndex: 2),
     Question(question: "What is 315 - 102", answerOne: "213", answerTwo: "417", answerThre: "217", answerFour: "413", correctAnswerIndex: 0),
-    Question(question: "What is 99 / 3", answerOne: "3", answerTwo: "30", answerThre: "33", answerFour: "9", correctAnswerIndex: 2)
+    Question(question: "What is 99 / 3", answerOne: "3", answerTwo: "30", answerThre: "33", answerFour: "9", correctAnswerIndex: 2),
+    Question(question: "What is 30 X 3", answerOne: "99", answerTwo: "90", answerThre: "33", answerFour: "9", correctAnswerIndex: 1)
 ]
