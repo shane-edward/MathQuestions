db482f13e41a3d2c962439948e0e9a3ab9771cfe
Shane Blackburn <shane_blackburn@icloud.com>
2020-05-18 18:00:02 -0400

    Added more questions to the questions array.

--- QuestionsAndAnswers/Questions.swift
+++ QuestionsAndAnswers/Questions.swift
@@ -19,4 +19,7 @@ struct Question {
 
 let questions = [
     Question(question: "What is 4 + 4", answerOne: "8", answerTwo: "10", answerThre: "0", answerFour: "4", correctAnswerIndex: 0),
+    Question(question: "What is the Square Root of 49", answerOne: "6", answerTwo: "2401", answerThre: "7", answerFour: "4.9", correctAnswerIndex: 2),
+    Question(question: "What is 315 - 102", answerOne: "213", answerTwo: "417", answerThre: "217", answerFour: "413", correctAnswerIndex: 0),
+    Question(question: "What is 99 / 3", answerOne: "3", answerTwo: "30", answerThre: "33", answerFour: "9", correctAnswerIndex: 2)
 ]
