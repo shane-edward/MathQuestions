e55ecb8c816cea46629c6c1d588018765a1418de
Shane Blackburn <shane_blackburn@icloud.com>
2020-05-17 23:43:21 -0400

    Initial Commit

---
+++ QuestionsAndAnswers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuestionsAndAnswers
+//
+//  Created by Shane Blackburn on 5/17/20.
+//  Copyright © 2020 Shane Blackburn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
