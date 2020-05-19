3d50481b470607d614255c1b4711faaadfdb3c7e
Shane Blackburn <shane_blackburn@icloud.com>
2020-05-18 20:05:56 -0400

    Structured basic UI for the app

--- QuestionsAndAnswers/ContentView.swift
+++ QuestionsAndAnswers/ContentView.swift
@@ -10,7 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Math is your friend")
+                Spacer()
+                Text("What is 2 + 2")
+                Spacer()
+                VStack(spacing: 20) {
+                    HStack(spacing: 20) {
+                        ForEach(0 ..< 2) { number in
+                            Button(action: {
+                                // flag was tapped
+                            }) {
+                                Text("4")
+                                    .frame(width: 100, height: 100)
+                                    .background(LinearGradient(gradient: Gradient(colors: [.white, .gray]), startPoint: .top, endPoint: .bottom))
+                                    .foregroundColor(.black)
+                            }
+                        }
+                    }
+                    HStack(spacing: 20) {
+                        ForEach(0 ..< 2) { number in
+                            Button(action: {
+                                // flag was tapped
+                            }) {
+                                Text("4")
+                                    .frame(width: 100, height: 100)
+                                    .background(LinearGradient(gradient: Gradient(colors: [.white, .gray]), startPoint: .top, endPoint: .bottom))
+                                    .foregroundColor(.black)
+                            }
+                        }
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
